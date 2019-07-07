fad5223bc0b4da3d8ab799fb4beb1ba23432668f
stott531 <stephenott531@gmail.com>
2019-07-07 11:55:58 -0700

    Cleaned up empty JSON files, removed unnecsaarry imports, and added more comments

--- BEETS/main.cpp
+++ BEETS/main.cpp
@@ -5,14 +5,10 @@
 
 int main(int argc, char *argv[])
 {
-    testSuite restored("foo_name");
 
     QApplication a(argc, argv);
     welcome_window w;
     w.show();
-
-
-
 
     return a.exec();
 }
