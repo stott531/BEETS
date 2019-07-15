18b17174239da647061345d9ec8fa611a6e2060a
stott531 <stephenott531@gmail.com>
2019-07-15 13:23:54 -0700

    Finished making example programe

---
+++ exampleProgram/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstring>
+
+/* This simple program is for testing purposes
+ *
+ * It finds the area of a circle or a rectangle
+ * Circle or square is specified in command line args
+ * Dimensions will be passed in via stdin
+ * Area will be returned via stdout
+ */
+
+int main(int argc, char** argv)
+{
+    if(strncmp(argv[1], "circle", 6) == 0)
+    {
+        double radius;
+        std::cin >> radius;
+
+        std::cout << radius * 3.14159265359;
+    }
+    else if(strncmp(argv[1], "square", 6) == 0)
+    {
+        double side;
+        std::cin >> side;
+
+        std::cout << side * side;
+    }
+    else
+    {
+        std::cout << "Invalid input";
+    }
+    
+    return 0;
+}
